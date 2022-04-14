201112c3d86e5d259bb99a03a9af9c104c453dd5
Kazuki Matsuda <mats.kazuki@gmail.com>
2022-04-14 11:11:47 +0000

    update to cdk v2.20.0

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.20.0',
   defaultReleaseBranch: 'main',
   name: 'rss-sumally',
 
